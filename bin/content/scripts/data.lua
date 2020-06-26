f249c013ba18d7da7afef5dc4c4f906c79bcf4e9
Thomas Leveille <thomas.leveille@younited-credit.fr>
2020-06-26 19:51:17 +0200

    review messages and remove useless lua files

--- bin/content/scripts/data.lua
+++ bin/content/scripts/data.lua
@@ -1,11 +1,11 @@
-print("[LUA]   Lua start !")
+print("[LUA]    Lua start !")
 
 -- Load templates
 print("[LUA]    Load Libraries")
 require("utils/data-loader")
 require("templates/player")
 
-print("[LUA]   Generate C++ Accessor")
+print("[LUA]    Generate C++ Accessor")
 function getobject(category, name)
     local object = data.raw[category][name]
     return object
--- bin/content/scripts/utils/data-loader.lua
+++ bin/content/scripts/utils/data-loader.lua
@@ -4,17 +4,17 @@ local table_string = "table"
 
 function data.extend(self, otherdata)
     if type(otherdata) ~= table_string or #otherdata == 0 then
-        error("Invalid prototype array ")
+        error("[LUA]    Invalid prototype array ")
     end
 
     for _, e in ipairs(otherdata) do
 
         if not e.type then
-            error("Missing type in the following prototype definition")
+            error("[LUA]    Missing type in the following prototype definition")
         end
 
         if not e.name then
-            error("Missing name in the following prototype definition")
+            error("[LUA]    Missing name in the following prototype definition")
         end
 
         local t = self.raw[e.type]
@@ -23,7 +23,6 @@ function data.extend(self, otherdata)
             self.raw[e.type] = t
         end
         t[e.name] = e
-
-        print("Lua dataloader (add):" .. e.type .. " | " .. e.name)
+        print("[LUA]    NEW PROTOTYPE - category:" .. e.type .. " | name:" .. e.name)
     end
 end
